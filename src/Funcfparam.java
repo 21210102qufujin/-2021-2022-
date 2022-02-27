22096ab99c4b8ae784e3a2fbd049c43b6127427e
21210102qufujin <91322790+21210102qufujin@users.noreply.github.com>
2022-02-27 19:54:07 +0800

    Create Funcfparam.java

---
+++ src/Funcfparam.java
@@ -0,0 +1,14 @@
+public class Funcfparam {
+    String old_name;
+    String paramtype;
+    String new_name;
+    int type;
+
+    Funcfparam(String old_name, String new_name, String paramtype , int type){
+        this.old_name=old_name;
+        this.new_name=new_name;
+        this.paramtype=paramtype;
+        this.type=type;
+    }
+
+}
